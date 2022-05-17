2c0d466a0ca22bbc398ccff15137ab49a96e7782
Jordy <castorena.jordy2@gmail.com>
2022-05-17 15:58:26 -0600

    Completed Objective5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,14 @@
+public class Objective5Lab1 {
+public static void main(String[] atgs) {
+        int num1 = 5;
+        int num2 = 15;
+
+        if(num1 > num2)  {
+                System.out.println(num1 + " is greater than " + num2);
+                //This code block will never execute because the values are hard coded.
+        }
+        else if(num2 > num1) {
+                System.out.println(num2 + " is greater than " + num1);
+        }
+}
+}
