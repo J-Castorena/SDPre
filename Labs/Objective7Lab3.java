ed169f8fa728f31bd8e149bf9f8b7600c2e841a6
Jordy <castorena.jordy2@gmail.com>
2022-05-19 15:51:55 -0600

    Completed Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,17 @@
+public class Objective7Lab3 {
+public static void main(String[] args) {
+        int counter = 1;
+
+        while(counter <= 20) {
+                if(counter % 2 == 0) {
+                        System.out.println( counter + " is even ");
+                } else if(counter % 2 == 1) {
+                        System.out.println( counter + " is odd ");
+                } else {
+                        System.out.println("The number is neutral ");
+                }
+                counter = counter + 1;
+        }
+
+}
+}
